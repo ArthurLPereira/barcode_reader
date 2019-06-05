8654abf8229ad744f28b644c8ccfa81161a65252
Arthur <gloriouslordbear@outlook.com>
2019-06-05 07:46:15 -0300

    Comentando e mais casos teste

--- barcode_reader.py
+++ barcode_reader.py
@@ -5,9 +5,8 @@ import cv2
 import sys
 
 class BarcodeFinder():
-    # self.image = None
-    # self.PATH = ''
-
+    
+    # Criando instância da classe
     def __init__(self, path):
         self.image = cv2.imread(path)
         path_split = path.split('/') if '/' in path else path.split('\\') if '\\' in path else ['', path]
@@ -15,12 +14,14 @@ class BarcodeFinder():
         self.PATH = '/'.join(path_split[:-1]) + '/'
         self.ddepth = cv2.CV_32F
     
+    # Convertendo a Imagem para escala de cinza
     def grayscale(self, image=None):
         if image is None:
             image = self.image
         grayscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         return grayscale
 
+    # Aplicando Sobel e suavisando
     def sobel(self, grayscale=None):
         if grayscale is None:
             grayscale = self.grayscale()
@@ -32,6 +33,7 @@ class BarcodeFinder():
         blurred = cv2.blur(gradient, (9, 9))
         return blurred
 
+    # Limiarizando a Imagem
     def threshold(self, blurred=None):
         if blurred is None:
             blurred = self.sobel()
@@ -39,6 +41,7 @@ class BarcodeFinder():
         _, threshold = cv2.threshold(blurred, 220, 255, cv2.THRESH_BINARY)
         return threshold
 
+    # Aplicando Erosão e Dilatação Várias Vezes
     def erode_dilate(self, threshold=None):
         if threshold is None:
             threshold = self.threshold()
@@ -49,20 +52,21 @@ class BarcodeFinder():
 
         return closed
 
+    # Encontrando o Retângulo que contem um possível código de barras
     def find_rectangle(self, closed=None):
         if closed is None:
             closed = self.erode_dilate()
         
         contours = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = imutils.grab_contours(contours)
-        # print(contours)
         c = sorted(contours, key=cv2.contourArea, reverse=True)[0]
 
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         return box
-    
+
+    # Aplicando retângulo na imagem original    
     def final_product(self, box=None):
         if box is None:
             box = self.find_rectangle()
@@ -90,47 +94,3 @@ if __name__ == "__main__":
     except Exception as exp:
         print(exp)
     pass
-
-
-
-# imagem = cv2.imread('./images/barcode_01.jpg')
-# cv2.imshow("Image", imagem)
-# cv2.waitKey(0)
-# escala_de_cinza = cv2.cvtColor(imagem, cv2.COLOR_RGB2GRAY)
-# cv2.imshow("Image", escala_de_cinza)
-# cv2.waitKey(0)
-# ddepth = cv2.CV_32F
-# gradienteX = cv2.Sobel(escala_de_cinza, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-# gradienteY = cv2.Sobel(escala_de_cinza, ddepth=ddepth, dx=0, dy=1, ksize=-1)
-
-# gradiente = cv2.subtract(gradienteX, gradienteY)
-# gradiente = cv2.convertScaleAbs(gradiente)
-
-# suavisada = cv2.blur(gradiente, (9, 9))
-# cv2.imshow("Image", suavisada)
-# cv2.waitKey(0)
-# aux, limiar = cv2.threshold(suavisada, 200, 255, cv2.THRESH_BINARY)
-# cv2.imshow("Image", limiar)
-# cv2.waitKey(0)
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-# fechado = cv2.morphologyEx(limiar,cv2.MORPH_CLOSE, kernel)
-# cv2.imshow("Image", fechado)
-# cv2.waitKey(0)
-# # Erosão e dilatação
-# fechado = cv2.erode(fechado, None, iterations=4)
-# fechado = cv2.dilate(fechado, None, iterations=4)
-
-# cv2.imshow("Image", fechado)
-# cv2.waitKey(0)
-# contornos = cv2.findContours(fechado.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# contornos = imutils.grab_contours(contornos)
-# c = sorted(contornos, key=cv2.contourArea, reverse=True)[0]
-
-# ret = cv2.minAreaRect(c)
-# caixa = cv2.boxPoints(ret)
-# caixa = np.int0(caixa)
-# np.int
-
-# cv2.drawContours(imagem, [caixa], -1, (0, 255, 0), 3)
-# cv2.imshow("Image", imagem)
-# cv2.waitKey(0)
